7a7cc88ca62719b132b414173e8f62aae1bd5e99
Changmin Lee <111254126+DiosAmor@users.noreply.github.com>
2023-09-11 16:32:20 +0900

    v2

--- Project6/cell.h
+++ Project6/cell.h
@@ -12,12 +12,54 @@ namespace MyExcel {
 		int x, y;
 		Table* table;
 
+	public:
+		virtual string stringify()=0;
+		virtual int to_numeric()=0;
+
+		Cell(int x, int y, Table* table);
+	};
+
+	class StringCell : public Cell {
 		string data;
 
 	public:
-		virtual string stringify();
-		virtual int to_numeric();
+		string stringify();
+		int to_numeric();
 
-		Cell(string data, int x, int y, Table* table);
+		StringCell(string data, int x, int y, Table* t);
+	};
+	class NumberCell : public Cell {
+		int data;
+
+	public:
+		string stringify();
+		int to_numeric();
+
+		NumberCell(int data, int x, int y, Table* t);
+	};
+	class DateCell :public Cell {
+		time_t data;
+
+	public:
+		string stringify();
+		int to_numeric();
+
+		DateCell(string s, int x, int y, Table* t);
+	};
+	class ExprCell :public Cell {
+		string data;
+		string* parsed_expr;
+
+		Vector exp_vec;
+
+		int precedence(char c);
+
+		void parse_expression();
+
+	public:
+		ExprCell(string data, int x, int y, Table* t);
+
+		string stringify();
+		int to_numeric();
 	};
 }
--- Project6/main.cpp
+++ Project6/main.cpp
@@ -6,13 +6,21 @@
 
 int main() {
 	MyExcel::TxtTable table(5, 5);
-	std::ofstream out("test.txt");
-
+	//std::ofstream out("test.txt");
+	/*
 	table.reg_cell(new MyExcel::Cell("Hello~!", 0, 0, &table), 0, 0);
 	table.reg_cell(new MyExcel::Cell("C++", 0, 1, &table), 0, 1);
 
 	table.reg_cell(new MyExcel::Cell("Programming", 1, 1, &table), 1, 1);
-	std::cout << std::endl << table;
-	out << table;
+	*/
+	table.reg_cell(new MyExcel::NumberCell(2, 1, 1, &table), 1, 1);
+	table.reg_cell(new MyExcel::NumberCell(3, 1, 2, &table), 1, 2);
+	table.reg_cell(new MyExcel::NumberCell(4, 2, 1, &table), 2, 1);
+	table.reg_cell(new MyExcel::NumberCell(5, 2, 2, &table), 2, 2);
+	table.reg_cell(new MyExcel::ExprCell("B2+B3*(C2+C3-2)", 3, 3, &table), 3, 2);
+	table.reg_cell(new MyExcel::StringCell("B2 + B3 * ( C2 + C3 - 2 ) = ", 3, 2, &table), 3, 1);
+
+	std::cout << table;
+	//out << table;
 	return 0;
 }
